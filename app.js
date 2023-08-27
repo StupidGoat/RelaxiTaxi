9063b860102e78863daed63e2d13c71712fcc676
ayush.jangid <ayush.jangid@flipkart.com>
2023-08-27 23:12:57 +0530

    setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Process = require("process");
+const app  = express();
+const PORT = Process.env.PORT || 5000;
+
+app.get("/", (req, res)=> {
+    res.send("Hey user! ");
+})
+
+const start = async ()=> {
+    try {
+        app.listen(PORT, () => {
+           console.log('Connected To ${PORT}');
+        });
+    } catch (error) {
+        console.log("error here in RootPage : ",error);
+    }
+};
+
+start();
---
+++ src/routes/product.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.route("/").get();
+router.route("/testHp").get();
+
+module.exports = router;
